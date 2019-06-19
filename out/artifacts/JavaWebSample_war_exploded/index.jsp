5efbe79a0324246645d5a5e417f6869f4932b5ad
liqingfeng <qingfeng-love@qq.com>
2019-06-19 17:43:24 +0800

    增加ignore文件

---
+++ out/artifacts/JavaWebSample_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 李青峰
+  Date: 2019/6/19
+  Time: 16:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello World!</title>
+</head>
+<body>
+    Hello! My first web application.
+</body>
+</html>
---
+++ src/com/github/stormwyrm/javawebsample/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package com.github.stormwyrm.javawebsample;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloWorldServlet",urlPatterns = {"/HelloWorldServlet"})
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.jsp");
+        requestDispatcher.forward(req,resp);
+    }
+}
